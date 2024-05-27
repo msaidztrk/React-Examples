a342eda9579d1557d375bb58b6a94db62c838ae2
Mehmet Said Öztürk <81260057+msaidztrk@users.noreply.github.com>
2024-05-27 10:34:58 +0300

    -

---
+++ duyu-customerOrder/frontend/src/utils/AuthContext.tsx
@@ -0,0 +1,77 @@
+import { useContext, useState, useEffect, createContext } from "react";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+
+const AuthContext = createContext<any | null>(null);
+
+interface User {
+  email: string;
+  password: string;
+}
+
+export const AuthProvider = ({ children }: any) => {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState<User | null>(null);
+
+  const { setLocalItem, getLocalItem, removeLocalItem } = useLocalStorage();
+
+  useEffect(() => {
+    checkUserStatus();
+  }, []);
+
+  const loginUser = async (email: string, password: string) => {
+    setLoading(true);
+
+    try {
+      if (email == "sa" && password == "as") {
+        setUser({ email, password }); // Set user data with type safety
+        setLocalItem("session", { email, password });
+      } else {
+        setUser(null);
+        setLocalItem("session", null);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+
+    setLoading(false);
+  };
+
+  const logoutUser = () => {
+    setUser(null);
+    removeLocalItem("session"); 
+
+    console.log('sa');
+  };
+
+  // const registerUser = (userInfo) => {}
+
+  const checkUserStatus = async () => {
+    try {
+      const local_val = getLocalItem("session");
+      setUser(local_val); // Set user data with type safety
+    } catch (err) {
+      console.log(err);
+    }
+
+    setLoading(false);
+  };
+
+  const contextData = {
+    user,
+    loginUser,
+    logoutUser,
+    // registerUser,
+  };
+
+  return (
+    <AuthContext.Provider value={contextData}>
+      {loading ? <p>Loading...</p> : children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export default AuthContext;
